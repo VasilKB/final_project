19fb77ad1874690698c4e5bd4549e5b26347cedb
Your Name <you@example.com>
2025-02-13 20:57:14 +0300

    first

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVISE = 'https://d84b40f6-3eb6-42df-b922-c8c0a16d24ed.serverhub.praktikum-services.ru'
+URL_SERVISE = 'https://64f79b3a-054d-46f1-9ec7-881ae827a269.serverhub.praktikum-services.ru'
 NEW_ORDER = '/api/v1/orders'
 GET_ORDER = '/api/v1/orders/track'
--- create_test.py
+++ create_test.py
@@ -1,14 +1,15 @@
 import requests
 import data
 import  configuration
-import  sender_stand
-
-def get_status_order():
-    return requests.get(configuration.URL_SERVISE + configuration.GET_ORDER,
-                         params = {'t' : sender_stand.get_order_track()},
-                         headers = data.headers)
-
-
+# Василиса Касьянова-Безниско, 26-я когорта — Финальный проект. Инженер по тестированию плюс
 def  test_positiv():
-    responce =get_status_order()
-    assert  responce.status_code == 200
+    def create_order(body):
+        return requests.post(configuration.URL_SERVISE + configuration.NEW_ORDER,
+                             json=body,
+                             headers=data.headers)
+
+    track = create_order(data.body).json()['track']
+    get_status_order = requests.get(configuration.URL_SERVISE + configuration.GET_ORDER,
+                        params={'t': track},
+                        headers=data.headers)
+    assert  get_status_order.status_code == 200
