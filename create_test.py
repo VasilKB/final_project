788040fc5e15e5ed35c2086551fea1c73a1745f8
VasilKB <146395901+VasilKB@users.noreply.github.com>
2025-02-24 12:18:32 +0300

    Update create_test.py

--- create_test.py
+++ create_test.py
@@ -4,7 +4,6 @@ import  configuration
 import sender_stand_request
 
 
-# Василиса Касьянова-Безниско, 26-я когорта — Финальный проект. Инженер по тестированию плюс
 def  test_positiv():
     track = sender_stand_request.create_order(data.body).json()['track']
     get_status = sender_stand_request.get_status_order(track)
